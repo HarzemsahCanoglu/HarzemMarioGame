d3b74d075a2d8fea2b40946aa10f6b1615d75221
Meliksah <n.m.canoglu@bites.com.tr>
2021-02-06 18:12:55 +0300

    Water Death Collision Added

---
+++ Assets/PlayerLifeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLifeController : MonoBehaviour
+{
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		
+		if (collision.gameObject.tag == "Water")
+		{
+			Debug.Log("We hit Water! And the tag of the water is: " + collision.gameObject.tag);
+
+			// Kill player
+			Destroy(gameObject);
+
+		}
+	}
+}
