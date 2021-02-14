241967dbca126e0fca1075b9250d44c1f0500315
Meliksah Canoglu <meliksah@mildmania.com>
2021-02-14 21:38:29 +0300

    Movement added without extra help from internet

---
+++ Assets/PMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMovement : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+          
+    Debug.Log("Space key was pressed.");
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Space key was released.");
+        }
+    }
+}
---
+++ Assets/PMovementv2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMovementv2 : MonoBehaviour
+{
+    public float thrust = 1.0f;
+    private Rigidbody2D rigidbody2;
+    public bool canJump = false;
+
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Hell yeah it works");
+            rigidbody2.AddForce(transform.right * -1 * thrust);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Hell yeah it works");
+            rigidbody2.AddForce(transform.right * thrust);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && canJump == true)
+        {
+            Debug.Log("Hell yeah it works");
+            rigidbody2.AddForce(transform.up * thrust);
+            canJump = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Debug.Log("Yeah BOIII");
+        
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("We're on the ground");
+            canJump = true;
+        }
+    }
+}
